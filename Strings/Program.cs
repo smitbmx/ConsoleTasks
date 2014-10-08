7c81f271543ae2e808e4b86dd4bc3c90fbeaff75
smitbmx <smitbmx@gmail.com>
2014-10-08 23:12:02 +0300

    Added 2 tasks on String project

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -12,7 +12,11 @@ namespace Strings
         {
             //Length();
 
-            CompareTo();
+            //CompareTo();
+
+            //Copy();
+
+            Split();
 
             Console.ReadLine();
         }
@@ -31,6 +35,36 @@ namespace Strings
             string l2 = Console.ReadLine();
 
             Console.WriteLine(l1.CompareTo(l2));
+        }
+
+        static void Copy()
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string temp = string.Copy(a);
+            a = String.Copy(b);
+            b = String.Copy(temp);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        static void Split()
+        {
+            string words = Console.ReadLine();
+
+            string[] arrWords = words.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            //foreach (string arrWord in arrWords)
+            //{
+            //    Console.WriteLine(arrWord);
+            //}
+
+            Console.WriteLine(arrWords[0]);
+            Console.WriteLine(arrWords[1]);
+            Console.WriteLine(arrWords[2]);
+            Console.WriteLine(arrWords[3]);
+            Console.WriteLine(arrWords[4]);
         }
     }
 }
