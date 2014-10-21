c2bed23bc228aba80ae9a69458944437e80f467b
smitbmx <smitbmx@gmail.com>
2014-10-21 19:44:35 +0300

    Added new Project and task about hours

---
+++ Lyrics/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyrics
+{
+    class Program
+    {
+        private delegate void RunMethod();
+
+        static void Main(string[] args)
+        {
+            RunMethod runMethod = Stub;
+            runMethod += StudentV;
+
+            runMethod.Invoke();
+
+            Console.ReadLine();
+        }
+
+        private static void Stub()
+        {
+
+        }
+
+        private static void StudentV()
+        {
+            string[] wakeUpTime = Console.ReadLine().Split(' ');
+            int hours = int.Parse(wakeUpTime[0]);
+            int minutes = int.Parse(wakeUpTime[1]);
+
+            int slept = hours*60 + minutes;
+            int left = 24*60 - slept;
+
+            Console.WriteLine(slept);
+            Console.WriteLine(left);
+        }
+    }
+}
