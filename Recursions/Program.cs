c5dfd7b01c133c5d2c0043cb97e5ec9d662b35b9
smitbmx <smitbmx@gmail.com>
2014-11-02 16:28:32 +0200

    Added project recursion

---
+++ Recursions/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursions
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Back();
+            Console.ReadLine();
+        }
+
+        private static void Stub()
+        {
+        }
+
+        private static int Factorial(int n)
+        {
+            return 0;
+        }
+
+        private static void Back()
+        {
+            string n = Console.ReadLine();
+            if (n == "")
+            {
+                return;
+            }
+            Back();
+            Console.WriteLine(n);
+        }
+    }
+}
