5469e737391434d054b62d4b345d49cf1616c254
smitbmx <smitbmx@gmail.com>
2014-11-02 23:13:15 +0200

    Added few tasks for the Math project

--- Math/Program.cs
+++ Math/Program.cs
@@ -12,7 +12,9 @@ namespace Math
         static void Main(string[] args)
         {
             RunMethod runMethod = Stub;
-            runMethod += Cube;
+            runMethod -= Cube;
+            runMethod -= Circle;
+            runMethod += StopWatch;
 
             runMethod.Invoke();
 
@@ -34,6 +36,40 @@ namespace Math
             int res = length * length * 4;
 
             Console.WriteLine(res);
+        }
+
+        /// <summary>
+        /// Дана длина окружности. Найти площадь данного круга. С точностью до сотых.
+        //В качестве числа π используйте стандартную константу PI.
+        //Начальные данные: одно действительные (вещественное) число - длина окружности.
+        //Вывод результата: одно действительное (вещественное) число - площадь круга.
+        /// </summary>
+        private static void Circle()
+        {
+            double circleLength = double.Parse(Console.ReadLine());
+            double radius = circleLength / (2.0 * System.Math.PI);
+
+            double circleSquare = System.Math.PI * radius * radius;
+
+            Console.WriteLine("{0:0.00}", circleSquare);
+        }
+
+        /// <summary>
+        /// Дано количество секунд с начала суток.
+        //Найти, сколько времени покажут часы через указанное количество секунд:
+        //часов (0-23), минут (0-59) и секунд (0-59).
+        //Начальные данные: одно натуральное число - количество секунд.
+        //Вывод результата: три натуральных числа на строке через пробел.
+        /// </summary>
+        private static void StopWatch()
+        {
+            long totalSeconds = long.Parse(Console.ReadLine());
+
+            long seconds = totalSeconds % 60;
+            long minutes = totalSeconds / 60 % 60;
+            long hours = totalSeconds / 3600 % 24;
+
+            Console.WriteLine(string.Concat(hours, " ", minutes, " ", seconds));
         }
     }
 }
