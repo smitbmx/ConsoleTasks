0c2e761c56b04de9767fa11dd3d6da638c5b36f3
smitbmx <smitbmx@gmail.com>
2014-11-02 19:34:30 +0200

    Added 3 tasks to Linear project

--- LinearRecord/Program.cs
+++ LinearRecord/Program.cs
@@ -13,7 +13,9 @@ namespace LinearRecord
         static void Main(string[] args)
         {
             RunMethod runMethod = Stub;
-            runMethod += Formula0000;
+            runMethod -= Formula0000;
+            runMethod -= Formula0001;
+            runMethod += Formula0010;
 
             runMethod.Invoke();
 
@@ -44,7 +46,38 @@ namespace LinearRecord
         /// </summary>
         private static void Formula0000()
         {
+            double x = double.Parse(Console.ReadLine());
 
+            Console.WriteLine("{0:0.000}", Math.Abs(x));
+            Console.WriteLine("{0:0.000}", Math.Sin(x));
+            Console.WriteLine("{0:0.000}", Math.Cos(x));
+            Console.WriteLine("{0:0.000}", Math.PI * x);
+            Console.WriteLine("{0:0.000}", Math.Pow(x, 2));
+            Console.WriteLine("{0:0.000}", Math.Sqrt(x));
+            Console.WriteLine("{0:0.000}", Math.Log(x));
+            Console.WriteLine("{0:0.000}", Math.Log10(x));
+            Console.WriteLine("{0:0.000}", Math.Exp(x));
+            Console.WriteLine("{0:0.000}", Math.Pow(x, Math.E));
+        }
+
+        private static void Formula0001()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+
+            double y = (Math.Pow((Math.Abs(a - x * x)) * Math.Log(a + x), 1.0 / 3.0)) / ((Math.Pow(x * x, 1.0 / 3.0)) + (Math.Pow(a, 1.0 / 5.0)));
+
+            Console.WriteLine("{0:0.00}", y);
+        }
+
+        private static void Formula0010()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double x = double.Parse(Console.ReadLine());
+
+            double z = (Math.Pow(x * x * x, 1 / 4.0) + a * x) / (Math.Log(Math.Sqrt(a * a + Math.Sqrt(x))));
+
+            Console.WriteLine("{0:0.0000}", z);
         }
     }
 }
