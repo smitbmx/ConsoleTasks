12768d5fb66da5b5eec3001bf79ffeaa9f6c9acc
smitbmx <smitbmx@gmail.com>
2014-11-14 22:31:28 +0200

    Added new task Arithmetic

--- FullTasks/Program.cs
+++ FullTasks/Program.cs
@@ -8,8 +8,54 @@ namespace FullTasks
 {
     class Program
     {
+        private delegate void RunMethod();
         static void Main(string[] args)
         {
+            RunMethod runMethod = Stub;
+            runMethod -= Arithmetic;
+            runMethod += Equation;
+
+            runMethod.Invoke();
+
+            Console.ReadLine();
+        }
+
+        private static void Stub() { }
+
+        /// <summary>
+        /// Вводится арифметическое выражение в виде: 
+        //A*B=
+        //Всегда именно в таком виде: 
+        //первое число, знак умножения, второе число, знак равно, без пробелов.
+
+        //Вычислить и вывести значение выражения.
+
+
+        //Начальные данные: арифметическое выражение с операцией умножения.
+        //Вывод результата: одно целое число - значение арифметического выражения.
+        /// </summary>
+        private static void Arithmetic()
+        {
+            string input = Console.ReadLine();
+            int starPosition = input.IndexOf('*');
+            int equalPosition = input.IndexOf('=');
+
+            long n1 = long.Parse(input.Substring(0, starPosition));
+            long n2 = long.Parse(input.Substring(starPosition + 1, equalPosition - starPosition - 1));
+
+            Console.WriteLine(n1 * n2);
+        }
+
+        /// <summary>
+        /// Вводятся коэффициенты а и b уравнения ax + b = 0 (a ≠ 0).
+        //Найти решение этого уравнения с точностью до десятых.
+        //Подсказка: x = - b / a.
+        //Начальные данные: два вещественных числа.
+        //Вывод результата: одно вещественное число - x.
+        /// </summary>
+        private static void Equation()
+        {
+
         }
     }
 }
