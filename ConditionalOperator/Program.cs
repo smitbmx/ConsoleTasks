0785c56c403b242d5492f86af9f1a99bd57307b3
smitbmx <smitbmx@gmail.com>
2014-11-26 22:19:55 +0200

    Added new project "Conditional Operator"

---
+++ ConditionalOperator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        private delegate void RunMethod();
+        static void Main(string[] args)
+        {
+            RunMethod runMethod = null;
+            runMethod -= Stub;
+            runMethod += Maximum;
+
+            runMethod.Invoke();
+
+            Console.ReadLine();
+        }
+
+        private static void Stub(){}
+
+        private static void Maximum()
+        {
+            
+        }
+    }
+}
