829d6537f20ec45661bbfa6b360beb82b0b36f60
smitbmx <smitbmx@gmail.com>
2014-11-28 07:45:17 +0200

    Added first task to Conditional project

--- ConditionalOperator/Program.cs
+++ ConditionalOperator/Program.cs
@@ -20,11 +20,38 @@ namespace ConditionalOperator
             Console.ReadLine();
         }
 
-        private static void Stub(){}
+        private static void Stub() { }
 
+        /// <summary>
+        /// Вводятся 2 числа. 
+        //Опредилть максимальное из этих чисел.
+        //Если числа равны - выбрать любое.
+
+        //Решить задачу двумя способами:
+        //1. Используя условный оператор if
+        //2. Используя тринарный оператор ? :
+
+        //Начальные данные: два целых числа на двух строчках.
+        //Вывод результата: дважды максимальное число на двух строках.
+        /// </summary>
         private static void Maximum()
         {
-            
+            long first = long.Parse(Console.ReadLine());
+            long second = long.Parse(Console.ReadLine());
+
+            long bigger1 = first > second ? first : second;
+            long bigger2;
+            if (first > second)
+            {
+                bigger2 = first;
+            }
+            else
+            {
+                bigger2 = second;
+            }
+
+            Console.WriteLine(bigger1);
+            Console.WriteLine(bigger2);
         }
     }
 }
