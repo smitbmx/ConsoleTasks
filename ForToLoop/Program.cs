f0efab3abb2bdf03511a1b09cec42b328a9de8cb
smitbmx <smitbmx@gmail.com>
2014-12-09 21:42:13 +0200

    Added task TwoDigitNumbers

--- ForToLoop/Program.cs
+++ ForToLoop/Program.cs
@@ -12,9 +12,9 @@ namespace ForToLoop
         static void Main(string[] args)
         {
             RunMethod runMethod = Stub;
-            runMethod = Stars;
+            runMethod -= Stars;
             runMethod -= ThreeDigitNumbers;
-            //runMethod += TwoDigitNumbers;
+            runMethod += TwoDigitNumbers;
 
             runMethod.Invoke();
 
@@ -39,6 +39,19 @@ namespace ForToLoop
         private static void ThreeDigitNumbers()
         {
             for (int i = 100; i <= 999; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// <summary>
+        /// Вывести все двухзначные числа в порядке убывания, в столбик.
+        //Начальные данные: нет.
+        //Вывод результата: 90 чисел на 90 строчках.
+        /// </summary>
+        private static void TwoDigitNumbers()
+        {
+            for (int i = 99; i >= 10; i--)
             {
                 Console.WriteLine(i);
             }
