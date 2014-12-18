64a8fdaf5a36959c65590fd3bbef2e43bf116767
smitbmx <smitbmx@gmail.com>
2014-12-18 23:37:57 +0200

    Implemented "DYAlplabet" task

--- WhileLoop/Program.cs
+++ WhileLoop/Program.cs
@@ -13,7 +13,8 @@ namespace WhileLoop
         {
             RunMethod runMethod = Stub;
             runMethod -= AllNumbers;
-            runMethod += NegativeNumbers;
+            runMethod -= NegativeNumbers;
+            runMethod += DYAlplabet;
 
             runMethod.Invoke();
 
@@ -42,6 +43,23 @@ namespace WhileLoop
                 Console.WriteLine(i);
                 i--;
             }
+        }
+
+        /// <summary>
+        /// Вывести в строчку буквы английского алфавита от D до Y.
+        /// </summary>
+        private static void DYAlplabet()
+        {
+            char start = 'D';
+            char end = 'Y';
+            int i = start;
+            while (i <= end)
+            {
+                Console.Write(Convert.ToChar(i));
+                i++;
+            }
+
+            Console.WriteLine();
         }
     }
 }
